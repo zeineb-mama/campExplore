61e7623cb6b04f71a11ae86430e5552688998826
yassine <yassinkaabi14@gmail.com>
2024-09-11 23:41:21 +0200

    fix fb

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    }
+);
 
-const sequelize = new Sequelize('ecom', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-
-sequelize.authenticate()
+sequelize
+    .authenticate()
     .then(() => console.log('Connection has been established successfully.'))
-    .catch(err => console.error('Unable to connect to the database:', err));
-
+    .catch((err) => console.error('Unable to connect to the database:', err));
 
 module.exports = sequelize;
