20db162eecf937fbe8acfcddfb418d63df0ab548
yassine <yassinkaabi14@gmail.com>
2024-09-12 00:32:30 +0200

    fix server

--- backend/index.js
+++ backend/index.js
@@ -35,9 +35,11 @@ app.get("/", (req, res) => {
 
 // Pour le front standard
 app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
+
 
 // Pour l'espace admin
 // app.use('/admin', express.static(path.join(__dirname, '../admin/build')));
