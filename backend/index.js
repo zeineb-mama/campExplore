3b26ca1e4b02183c9b8b5e688b90724136e57833
yassine <yassinkaabi14@gmail.com>
2024-09-12 22:38:38 +0200

    make it better

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const sequelize = require('./src/config/db');
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://camp-explore-a1583b8ebde7.herokuapp.com"
+  origin: "*"
 }));
 
 sequelize.sync();
