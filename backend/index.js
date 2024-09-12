16b8312947c4f655e36779288a3f2316cd058896
yassine <yassinkaabi14@gmail.com>
2024-09-12 14:31:47 +0200

    fix build

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
 // Pour le front standard
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 
